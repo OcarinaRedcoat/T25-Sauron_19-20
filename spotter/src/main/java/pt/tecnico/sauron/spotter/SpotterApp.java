657f5c1bd8b2b1703107c9ec1304398eba00a239
Caetano <98.ricardo.caetano@gmail.com>
2020-03-31 12:51:33 +0100

    Added a summary of what comes next

--- spotter/src/main/java/pt/tecnico/sauron/spotter/SpotterApp.java
+++ spotter/src/main/java/pt/tecnico/sauron/spotter/SpotterApp.java
@@ -1,8 +1,12 @@
 package pt.tecnico.sauron.spotter;
 
 
+import pt.tecnico.sauron.silo.client.SiloFrontend;
+
 public class SpotterApp {
-	
+
+	private static SiloFrontend library;
+
 	public static void main(String[] args) {
 		System.out.println(SpotterApp.class.getSimpleName());
 		
@@ -11,6 +15,19 @@ public class SpotterApp {
 		for (int i = 0; i < args.length; i++) {
 			System.out.printf("arg[%d] = %s%n", i, args[i]);
 		}
+
+		String[] hpToLib = new String[2]; //hot and port to Library (SiloFrontend library)
+		hpToLib[0] = args[1]; hpToLib[1] = args[2];
+		library = new SiloFrontend(hpToLib);
+
+
+		while(true){
+			// fica as espera de uma entrada
+			// deve fazer sempre a verificacao de erros
+			// spot -> pode ser track ou trackMatch dependendo da existencia de *
+			// trail apenas pode ser trace com ou sem *
+		}
+
 	}
 
 }
