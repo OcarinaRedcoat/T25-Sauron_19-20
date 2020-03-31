7c4b3faa87960936bbdf3cbfba95ade74e684b3b
Caetano <98.ricardo.caetano@gmail.com>
2020-03-31 12:50:41 +0100

    Added a summary of what comes next

--- eye/src/main/java/pt/tecnico/sauron/eye/EyeApp.java
+++ eye/src/main/java/pt/tecnico/sauron/eye/EyeApp.java
@@ -6,6 +6,7 @@ import pt.tecnico.sauron.silo.client.SiloFrontend;
 public class EyeApp {
 
 	private static SiloFrontend library;
+	private static String name;
 
 	public static void main(String[] args) {
 		System.out.println(EyeApp.class.getSimpleName());
@@ -22,10 +23,19 @@ public class EyeApp {
 		hpToLib[0] = args[1]; hpToLib[1] = args[2];
 		library = new SiloFrontend(hpToLib);
 
+		name = args[2]; // camera name
+
 		float latitude = Float.parseFloat(args[3]);
 		float longitude = Float.parseFloat(args[4]);
 
 		library.camJoin(args[2], latitude, longitude); // args[2] -> name
 
+		while (true){
+			//loop de à espera de entradas
+			//
+			// quando recebe argumentos faz verificacao e manda excecoes s enecessario
+			// fazer verificacao de comentario, e sleep, exp # e zzz
+			// caso contratio chama a library.report(type , id , this.name ) -> nome da camara
+		}
 	}
 }
