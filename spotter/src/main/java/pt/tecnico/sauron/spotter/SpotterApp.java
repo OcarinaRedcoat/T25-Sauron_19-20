23a90fc846ecb4305a8407d5f26f8fc785296f8c
NikonPT <pedro.r.teixeira@tecnico.ulisboa.pt>
2020-04-01 18:47:02 +0100

    corrected scanner SpotterApp

--- spotter/src/main/java/pt/tecnico/sauron/spotter/SpotterApp.java
+++ spotter/src/main/java/pt/tecnico/sauron/spotter/SpotterApp.java
@@ -22,59 +22,78 @@ public class SpotterApp {
 
 		//SiloFrontend library = new SiloFrontend(args);
 
-		Scanner scanner = new Scanner("Welcome to SpotterApp, type in you question");
-		System.out.println("Antes do while");
+//		Scanner scanner = new Scanner("Welcome to SpotterApp, type in you question");
+//		System.out.println("Antes do while");
 
-		//while(true) {
-		while(scanner.hasNext()){
-			System.out.println("detro do while");
 
-			String command = scanner.next();	// spot ou trail
-			String info = scanner.nextLine();	// example: person 26r8723 ou car 67890*
-			int size = info.length();			// size of info
 
-			if(command.equals("spot")) {		// spot: track or trackMatch, depending on having *
-				System.out.println("é spot");
-				if(info.startsWith("person") & info.substring(7, size).contains("*")) {
-					System.out.println("spot person COM asterisco");
-					//library.trackMatch("person", info.substring(7, size));
+
+		try (Scanner scanner = new Scanner(System.in)){
+
+			//while(true) {
+			while(scanner.hasNextLine()){
+				System.out.println("detro do while");
+
+				String command = scanner.next();	// spot ou trail
+				String info = scanner.nextLine();	// example: person 26r8723 ou car 67890*
+				int size = info.length();			// size of info
+
+				if(command.equals("spot")) {		// spot: track or trackMatch, depending on having *
+					System.out.println("é spot");
+					if(info.startsWith("person") & info.substring(7, size).contains("*")) {
+						System.out.println("spot person COM asterisco");
+						//library.trackMatch("person", info.substring(7, size));
+					}
+					else if (info.startsWith("car") & info.substring(4, size).contains("*")) {
+						System.out.println("spot car COM asterisco");
+						//library.trackMatch("car", info.substring(4, size));
+					}
+					else if(info.startsWith("person") & !info.substring(7, size).contains("*")) {
+						System.out.println("spot person SEM asterisco");
+						//library.track("person", info.substring(7, size));
+					}
+					else if (info.startsWith("car") & !info.substring(4, size).contains("*")) {
+						System.out.println("spot car SEM asterisco");
+						//library.track("car", info.substring(4, size));
+					}
+					else {
+						System.out.println(("Deves ter escrito mal o spot, pá"));	//FIXME exceção?
+					}
 				}
-				else if (info.startsWith("car") & info.substring(4, size).contains("*")) {
-					System.out.println("spot car COM asterisco");
-					//library.trackMatch("car", info.substring(4, size));
-				}
-				else if(info.startsWith("person") & !info.substring(7, size).contains("*")) {
-					System.out.println("spot person SEM asterisco");
-					//library.track("person", info.substring(7, size));
-				}
-				else if (info.startsWith("car") & !info.substring(4, size).contains("*")) {
-					System.out.println("spot car SEM asterisco");
-					//library.track("car", info.substring(4, size));
+				else if (command.equals("trail")) {		// trail: only trace
+					System.out.println("é trail");
+					if(info.startsWith("person")) {
+						System.out.println("trail person");
+						//library.trace("person", info.substring(7, size));
+					}
+					else if (info.startsWith("car")) {
+						System.out.println("trail car");
+						//library.trace("car", info.substring(4, size));
+					}
+					else {
+						System.out.println(("Deves ter escrito mal o trail, pá"));	//FIXME exceção?
+					}
 				}
 				else {
-					System.out.println(("Deves ter escrito mal o spot, pá"));	//FIXME exceção?
+					System.out.println(("Exceção, i guess"));	//FIXME exceção?
 				}
-			}
-			else if (command.equals("trail")) {		// trail: only trace
-				System.out.println("é trail");
-				if(info.startsWith("person")) {
-					System.out.println("trail person");
-					//library.trace("person", info.substring(7, size));
-				}
-				else if (info.startsWith("car")) {
-					System.out.println("trail car");
-					//library.trace("car", info.substring(4, size));
-				}
-				else {
-					System.out.println(("Deves ter escrito mal o trail, pá"));	//FIXME exceção?
-				}
-			}
-			else {
-				System.out.println(("Exceção, i guess"));	//FIXME exceção?
+
 			}
 
 		}
 
+
+
+
+
+
+
 	}
+
+
+
+
+
+
 
 }
