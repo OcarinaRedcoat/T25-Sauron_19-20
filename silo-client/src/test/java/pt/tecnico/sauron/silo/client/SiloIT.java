f13633c951455c3ee1cf812c4c92b85da1eb5e58
NikonPT <pedro.r.teixeira@tecnico.ulisboa.pt>
2020-04-29 23:31:12 +0100

    TESTS: reportINVALID_ARGUMENT trackNOT_FOUND done

--- silo-client/src/test/java/pt/tecnico/sauron/silo/client/SiloIT.java
+++ silo-client/src/test/java/pt/tecnico/sauron/silo/client/SiloIT.java
@@ -4,10 +4,10 @@ import io.grpc.StatusRuntimeException;
 import org.junit.jupiter.api.*;
 import pt.tecnico.sauron.silo.grpc.SiloOuterClass;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import static io.grpc.Status.ALREADY_EXISTS;
-import static io.grpc.Status.INVALID_ARGUMENT;
+import static io.grpc.Status.*;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -22,6 +22,8 @@ public class SiloIT extends BaseIT {
 	// one-time initialization and clean-up
 	@BeforeAll
 	public static void oneTimeSetUp() {
+		frontEnd = new SiloFrontend();
+		frontEnd.createChannel("localhost", "8080");
 	}
 
 	@AfterAll
@@ -32,23 +34,13 @@ public class SiloIT extends BaseIT {
 
 	@BeforeEach
 	public void setUp() {
-
-		frontEnd = new SiloFrontend();
-		frontEnd.createChannel("localhost", "8080");
 	}
 
 	@AfterEach
 	public void tearDown() {
-
 		frontEnd.ctrlClear();
 	}
 
-
-	@Test
-	public void pingOKTest() {
-		//SiloOuterClass.PingRequest request = SiloOuterClass.PingRequest.newBuilder().setPing("ping").build();
-		assertEquals("ping pong!", frontEnd.ctrlPing("ping"));
-	}
 		
 	// test T1
 
@@ -58,10 +50,10 @@ public class SiloIT extends BaseIT {
 	}
 
 	@Test
-	public void duplicateCam() {
+	public void duplicateCamJoin() {
 		frontEnd.camJoin("Tagus", "10.0", "20.0");
-		assertEquals(ALREADY_EXISTS.getDescription(),
-				assertThrows(StatusRuntimeException.class, () -> frontEnd.camJoin("Tagus", "15.0", "30.0")).getStatus().getDescription());
+		assertEquals(ALREADY_EXISTS.getCode(),
+				assertThrows(StatusRuntimeException.class, () -> frontEnd.camJoin("Tagus", "15.0", "30.0")).getStatus().getCode());
 	}
 
 	@Test
@@ -70,12 +62,6 @@ public class SiloIT extends BaseIT {
 		frontEnd.camInfo("Tagus");
 	}
 
-	/*@Test
-	public void nullCamInfo() {
-		frontEnd.camJoin("Tagus", "10.0", "20.0");
-		assertEquals(ALREADY_EXISTS.getDescription(),
-				assertThrows(StatusRuntimeException.class, () -> frontEnd.camJoin("Tagus", "15.0", "30.0")).getStatus().getDescription());
-	}*/
 
 	@Test
 	public void reportOK() {
@@ -86,19 +72,48 @@ public class SiloIT extends BaseIT {
 	}
 
 	@Test
+	public void reportINVALID_ARGUMENT() {
+		frontEnd.camJoin("Tagus", "10.0", "20.0");
+
+		List<String> types = List.of("car", "person", "person");
+		List<String> ids = List.of("HJ23FG123123", "11", "12");
+
+		assertEquals(INVALID_ARGUMENT.getCode(),
+				assertThrows(StatusRuntimeException.class, () -> frontEnd.report(types, ids, "Tagus")).getStatus().getCode());
+	}
+
+	@Test
 	public void trackOK() {
 		frontEnd.camJoin("Tagus", "10.0", "20.0");
 
-		List<String> types = List.of("car");
-		List<String> ids = List.of("HJ23FG");
+		List<String> types = List.of("car", "person");
+		List<String> ids = List.of("HJ23FG", "12345");
 
 		frontEnd.report(types, ids, "Tagus");
-		String obs = frontEnd.track(types.get(0), ids.get(0));
+		String obs1 = frontEnd.track(types.get(0), ids.get(0));
+		String obs2 = frontEnd.track(types.get(1), ids.get(1));
 
-		int obs_size = types.get(0).length() + ids.get(0).length() + 1;
+		int obs1_size = types.get(0).length() + ids.get(0).length() + 1;
+		int obs2_size = types.get(1).length() + ids.get(1).length() + 1;
 
 		// provavelmente precisa de for para percorrer lista
-		assertEquals(types.get(0) + ',' + ids.get(0), obs.substring(0,obs_size));
+		assertEquals(types.get(0) + ',' + ids.get(0), obs1.substring(0,obs1_size));
+		assertEquals(types.get(1) + ',' + ids.get(1), obs2.substring(0,obs2_size));
+
+	}
+
+	/* ee redudante ter invalid argument  no track pois o report ja trata dessa exception */
+
+	@Test
+	public void trackNOT_FOUND() {
+		frontEnd.camJoin("Tagus", "10.0", "20.0");
+
+		List<String> types = List.of("car", "person");
+		List<String> ids = List.of("HJ23FG", "12345");
+
+		frontEnd.report(types, ids, "Tagus");
+
+		assertEquals(NOT_FOUND.getCode(), assertThrows(StatusRuntimeException.class, () -> frontEnd.track("car", "7479RV")).getStatus().getCode());
 	}
 
 	/*@Test
@@ -106,14 +121,21 @@ public class SiloIT extends BaseIT {
 
 		frontEnd.camJoin("Tagus", "10.0", "20.0");
 
-		List<String> types = List.of("car");
-		List<String> ids = List.of("HJ23FG");
+		List<String> types = List.of("car", "car");
+		List<String> ids = List.of("HJ23FG", "7479RV");
 
 		frontEnd.report(types, ids, "Tagus");
+		String observations = frontEnd.trackMatch(types.get(0), "*");
 
+		int obs_size = types.get(0).length() + ids.get(0).length() + 1;
+		assertEquals(types.get(1) + ',' + ids.get(1), observations.substring(0,obs_size));
+
+		System.out.println(ind);
+		System.out.println(observations.substring(0, ind));
 
 
 	}*/
+
 
 
 
